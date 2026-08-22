2330bc2da1e0065ea7615683e34537e1c4721665
GPRETAIL <vinoth1300@gmail.com>
2026-08-23 02:27:16 +0530

    feat(mobile): route /app to enterprise transaction workspace

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import ActivationGate from "./components/ActivationGate";
 import ActivationWizard from "./pages/ActivationWizard";
 import SetPassword from "./pages/SetPassword";
 import ForceChangePassword from "./pages/ForceChangePassword";
-import MobileAppV2 from "./mobile/MobileAppV2";
+import EnterpriseMobileWorkspace from "./mobile/EnterpriseMobileWorkspace";
 
 function App() {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
@@ -33,7 +33,7 @@ function App() {
             <Route path="/set-password" element={<SetPassword />} />
             <Route path="/register" element={isAuthenticated ? <Navigate to={homePath} replace /> : <Register />} />
             <Route element={<ProtectedRoute />}>
-              <Route path="/app/*" element={<MobileAppV2 />} />
+              <Route path="/app/*" element={<EnterpriseMobileWorkspace />} />
               <Route path="*" element={<MainLayout />} />
             </Route>
             <Route path="/" element={isAuthenticated ? <Navigate to={homePath} replace /> : <Navigate to="/login" replace />} />
